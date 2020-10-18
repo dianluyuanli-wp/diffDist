b5791e223821247e68170439a052133284a3056a
xiaohuli <waterdoor@foxmail.com>
2020-10-18 17:28:08 +0800

    fix: 初始化

---
+++ tag.js
@@ -0,0 +1,19 @@
+var process = require('child_process');
+
+//  子进程完成后的回调
+function cb(error, stdout, stderr) {
+  console.log(stdout);
+  if (error !== null) {
+    console.log('exec error: ' + error);
+  }
+}
+
+process.exec(`git add .`,cb);
+process.exec(`git commit -am "new version"`,cb);
+
+//  打tag
+process.exec(`git tag -a ${newVersion} -m "new version: ${newVersion}"`,cb);
+
+process.exec(`git push origin master`,cb);
+//  推tag
+process.exec(`git push origin ${newVersion}`,cb);
